44b71eb7a638e2f34738039f1c172341ec1273ba
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2021-01-25 16:14:34 +0100

    add media queries footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,10 +21,14 @@ const socialLinks = [
 
 const Footer = () => {
     return <div className="footer">
-       <div className="footer-container-social-link">
-       {socialLinks.map((socialLink, i) => 
-            <a className="footer-social-link-bg" href={socialLink.link} target={socialLink.target}>{socialLink.icon}</a>
-        )}
+       <div className="footer-container">
+            <a class="footer-navlink" href="#"> mentions légales</a>
+                <div className="footer-container-social-link">
+                {socialLinks.map((socialLink, i) => 
+                        <a className="footer-social-link-bg" href={socialLink.link} target={socialLink.target}>{socialLink.icon}</a>
+                    )}
+                </div>
+            <a class="footer-navlink" href="#"> contact</a>
        </div>
     </div>
 }
