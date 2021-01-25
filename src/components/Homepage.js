e790cbab053173ec3e328496a549cb55850c1cdd
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2021-01-25 18:03:27 +0100

    add homepage js and css without background image

---
+++ src/components/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Homepage.css";
+
+function Homepage() {
+    return (
+        <div className="homepage">
+        <h1 className="title">Réduisez votre empreinte carbone</h1> 
+        <div className="btn"><button>Simulator</button>
+        <button>Connexion</button></div>
+    </div>
+    )
+}
+
+
+
+
+export default Homepage;
