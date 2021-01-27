1e4e03bf7744c84e8a99c120b9b7ee5ddfbd49e4
RaphNokos <raph.nokos@outlook.com>
2021-01-27 17:04:20 +0100

    v1 solutions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Solutions from './components/Solutions'
+import CardsList from './components/CardsList'
 
 function App() {
   return (
     <div className="App">
-        
+      <Solutions />  
+      <CardsList />
     </div>
   );
 }
