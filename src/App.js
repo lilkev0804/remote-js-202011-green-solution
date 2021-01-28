356f4b85b3201d3f72638948d28c954cc339f860
INGRID <ingridpruvost966@gmail.com>
2021-01-28 17:29:54 +0100

    résolution des conflits

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
-import Titre from './components/Titre';
-import Equipe from './components/Equipe';
-import Objectif from './components/Objectif';
-import Info from './components/Info';
-import Coordonnées from './components/Coordonnées';
-=======
 import './App.css';
-import Solutions from './components/Solutions'
-import CardsList from './components/CardsList'
->>>>>>> d0712b6127cd4878741be03305e3002d38823e5c
+import Aboutus from "./screen/Aboutus";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Titre />
-      <Equipe 
-        name="Raphaëlle, Rachelle, Ingrid, Alex, Aurélien, Kévin,Marc" 
-      />
-      <Objectif />
-      <Info />
-      <Coordonnées />
+      <Aboutus />
     </div>
-    
-=======
-      <Solutions />  
-      <CardsList />
-    </div>
->>>>>>> d0712b6127cd4878741be03305e3002d38823e5c
   );
 }
 
