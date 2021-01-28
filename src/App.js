8b80dd8e87f3980e801c6812f8d0fb9a50e57f4a
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2021-01-28 18:44:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-<<<<<<< HEAD
+
 import './App.css'
 import Calculator from './Components/Calculator'
-=======
-<<<<<<< HEAD
 import './App.css';
-<<<<<<< HEAD
 import Body from './components/BodyResult';
 import Result from './components/Result';
 import Solution from './components/Solution';
 // import {BrowserRouter as Router, Route} from 'react-router-dom';
-=======
-=======
+
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
->>>>>>> 7c186de21f839036732256927ef5a40c792d8ebc
->>>>>>> dev
 import Solutions from './components/Solutions'
 import CardsList from './components/CardsList'
->>>>>>> d0712b6127cd4878741be03305e3002d38823e5c
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Calculator />
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
       <Body
         bodytext="Mon émission de CO2 pour le mois de janvier"
       />
@@ -45,23 +35,17 @@ function App() {
       {/* <Router>
         <Route path="/Solution" exact component={"page solution"}/>
       </Router> */}
-=======
->>>>>>> dev
+
       <Solutions />  
       <CardsList />
->>>>>>> d0712b6127cd4878741be03305e3002d38823e5c
-=======
+
       <Header />
       <Solutions />  
       <CardsList />
       <Footer />
->>>>>>> 7c186de21f839036732256927ef5a40c792d8ebc
+
     </div>
   )
 }
-
-<<<<<<< HEAD
-export default App
-=======
 export default App;
->>>>>>> dev
+export default App;
