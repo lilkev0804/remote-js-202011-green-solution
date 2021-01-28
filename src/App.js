7c06109aaa60862cad700f89f41ef4ff6eec1782
AlexandreRosier <alex.rosier1405@gmail.com>
2021-01-28 18:19:48 +0100

    resolve problem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-<<<<<<< HEAD
 import './App.css';
-<<<<<<< HEAD
 import Body from './components/BodyResult';
 import Result from './components/Result';
 import Solution from './components/Solution';
 // import {BrowserRouter as Router, Route} from 'react-router-dom';
-=======
-=======
-import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
->>>>>>> 7c186de21f839036732256927ef5a40c792d8ebc
 import Solutions from './components/Solutions'
 import CardsList from './components/CardsList'
->>>>>>> d0712b6127cd4878741be03305e3002d38823e5c
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-<<<<<<< HEAD
+      <Header />
       <Body
         bodytext="Mon émission de CO2 pour le mois de janvier"
       />
@@ -37,16 +29,9 @@ function App() {
       {/* <Router>
         <Route path="/Solution" exact component={"page solution"}/>
       </Router> */}
-=======
-      <Solutions />  
-      <CardsList />
->>>>>>> d0712b6127cd4878741be03305e3002d38823e5c
-=======
-      <Header />
       <Solutions />  
       <CardsList />
       <Footer />
->>>>>>> 7c186de21f839036732256927ef5a40c792d8ebc
     </div>
   );
 }
