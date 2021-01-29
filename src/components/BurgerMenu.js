4d1c8355d21f85eb10cfadb5e153853a8d723ccb
AlexandreRosier <alex.rosier1405@gmail.com>
2021-01-29 12:06:26 +0100

    test

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,4 +1,4 @@
-// import { bubble as Menu } from 'react-burger-menu'
+import { bubble as Menu } from 'react-burger-menu'
 import React from "react";
 import './BurgerMenu.css';
 
@@ -7,12 +7,12 @@ class BurgerMenu extends React.Component {
   render () {
     return (
 
-      <div className="menu-burger" left width={ '200px' } >
+      <Menu className="menu-burger" left width={ '200px' } >
         <a className="menu-item1" href="#">Home</a>
         <a className="menu-item2" href="#">Calculator</a>
         <a className="menu-item3" href="#">Solutions</a>
         <a className="menu-item4" href="#">About Us</a>
-      </div>
+      </Menu>
     );
   }
 }
