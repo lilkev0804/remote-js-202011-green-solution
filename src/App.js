4b909acd079266ee796045b57899b76d696561d3
AlexandreRosier <alex.rosier1405@gmail.com>
2021-01-29 09:36:47 +0100

    merge composant

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
-<<<<<<< HEAD
-=======
 
-import './App.css'
-import Calculator from './Components/Calculator'
->>>>>>> 4ce00210b00cddcd1f777622c3461c77a495c863
+import Calculator from './components/Calculator'
 import './App.css';
 import Body from './components/BodyResult';
 import Result from './components/Result';
 import Solution from './components/Solution';
 // import {BrowserRouter as Router, Route} from 'react-router-dom';
-<<<<<<< HEAD
-=======
-
-import './App.css'
->>>>>>> 4ce00210b00cddcd1f777622c3461c77a495c863
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Solutions from './components/Solutions'
 import CardsList from './components/CardsList'
-<<<<<<< HEAD
-=======
-
->>>>>>> 4ce00210b00cddcd1f777622c3461c77a495c863
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Header />
-=======
-      <Calculator />
->>>>>>> 4ce00210b00cddcd1f777622c3461c77a495c863
+      <Header/>
+      <Calculator/>
+      <Footer/>
+      <Header/>
       <Body
         bodytext="Mon émission de CO2 pour le mois de janvier"
       />
@@ -48,23 +34,15 @@ function App() {
       {/* <Router>
         <Route path="/Solution" exact component={"page solution"}/>
       </Router> */}
-<<<<<<< HEAD
-      <Solutions />  
-      <CardsList />
-      <Footer />
-=======
+      <Footer/>
 
-      <Solutions />  
-      <CardsList />
+      <Header/>
+      <Solutions/>  
+      <CardsList/>
+      <Footer/>
 
-      <Header />
-      <Solutions />  
-      <CardsList />
-      <Footer />
-
->>>>>>> 4ce00210b00cddcd1f777622c3461c77a495c863
     </div>
   )
 }
 export default App;
-export default App;
+
