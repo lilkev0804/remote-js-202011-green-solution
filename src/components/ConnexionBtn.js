209b452adcbf846e1748f737562ea12bf3f59b44
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2021-02-10 14:28:43 +0100

    css button

--- src/components/ConnexionBtn.js
+++ src/components/ConnexionBtn.js
@@ -4,7 +4,7 @@ import "./Homepage.css";
 function ConnexionBtn() {
     return (
         <div className="connexionBtn">
-            <button>Connexion</button>
+            <button className="buttonsHomepage">Connexion</button>
         </div>
     )
 }
