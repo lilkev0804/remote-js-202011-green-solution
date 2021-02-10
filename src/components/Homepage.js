f31b8967a20d131fd5e43f3db5683fbe4b005cb2
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2021-02-10 14:58:43 +0100

    img homepage changed

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,7 +6,9 @@ import "./Homepage.css";
 
 
 const Background = styled.section`
-background-image: url("./imgHomepage/homepage-mobile-bg.jpg");`
+background-image: url("./imgHomepage/homepageBg.jpg");
+background-size: cover;
+background-position: center;`
 
 function Homepage() {
     return (
@@ -14,7 +16,7 @@ function Homepage() {
             <Background className="imgHomepage2">
                 <div className="desktop">
                     <h1 className="homepage-title">Réduisez votre empreinte carbone</h1>
-                    <img className="imgHomepage" src="imgHomepage/homepage-desktop-bg.jpg" alt="road landscape"/>
+                    <img className="imgHomepage" src="imgHomepage/homepageBg.jpg" alt="road landscape"/>
                     <h4 className="textHomepage">Le temps est venu de connaître l'empreinte carbone de votre entreprise de transport.</h4>
                     <div className="homepage-btn">
                     <SimulatorBtn />
