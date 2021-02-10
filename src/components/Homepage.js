d23d4a3b119240c5fe7f441a7bb21d975f2037d0
Rachel-Ab <71349675+Rachel-Ab@users.noreply.github.com>
2021-02-10 10:16:47 +0100

    img + css homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import ConnexionBtn from './ConnexionBtn';
 import SimulatorBtn from './SimulatorBtn';
+import styled from "styled-components";
 import "./Homepage.css";
-import HomepageBg from './HomepageBg';
+
+
+const Background = styled.section`
+background-image: url("./imgHomepage/homepage-mobile-bg.jpg");`
 
 function Homepage() {
     return (
         <div className="homepage">
-            <h1 className="homepage-title">Réduisez votre empreinte carbone</h1> 
-            <HomepageBg />
-            <h4 className="textHomepage">Le temps est venu de connaître l'empreinte carbone de votre entreprise de transport.</h4>
-            <div className="homepage-btn">
-                <SimulatorBtn />
-                <ConnexionBtn />
-            </div>
+            <Background className="imgHomepage2">
+                <div className="desktop">
+                    <h1 className="homepage-title">Réduisez votre empreinte carbone</h1>
+                    <img className="imgHomepage" src="imgHomepage/homepage-desktop-bg.jpg" alt="road landscape"/>
+                    <h4 className="textHomepage">Le temps est venu de connaître l'empreinte carbone de votre entreprise de transport.</h4>
+                    <div className="homepage-btn">
+                    <SimulatorBtn />
+                    <ConnexionBtn />
+                    </div>
+                </div>
+            </Background>
             
         </div>
     )
 }
+
 
 export default Homepage;
