bcf794cff8620a9928761e47bef968cab09e8620
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2021-02-12 13:24:20 +0100

    add link to login in homepage

--- src/components/ConnexionBtn.js
+++ src/components/ConnexionBtn.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import "./Homepage.css";
+import {Link} from 'react-router-dom'
 
 function ConnexionBtn() {
     return (
         <div className="connexionBtn">
+            <Link to="/login" style={{textDecoration:'none'}}> 
             <button className="buttonsHomepage">Connexion</button>
+            </Link>
         </div>
     )
 }
