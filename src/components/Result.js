b4d065f5259de56635bcd6e0e75c2be1c6ca60b7
lilkev0804 <56228273+lilkev0804@users.noreply.github.com>
2021-02-17 14:22:17 +0100

    fixed error back and refresh from result page

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,42 +1,47 @@
+import React,{useState, useEffect} from "react";
+import { useHistory } from "react-router-dom";
 import BodyResult from "./BodyResult" 
 import Solution from "./Solution";
-import React from "react";
+
 import "./Result.css";
 
-class Result extends React.Component {
-  state = {
-    total: ''
-  }
-  catchValue = (selectEmissionFactors, distance, weight) => {
+
+
+function Result (props) {
+  const [total, setTotal] = useState('')
+  let history = useHistory();
+  const catchValue = (selectEmissionFactors, distance, weight) => {
     const vehicule = selectEmissionFactors.replace(',' , ".")
     const inputKm = distance.replace(',' , ".")
     const inputWeight = weight.replace(',' , ".")
     const userResult = vehicule * inputKm * inputWeight
-    this.setState({total: userResult})
+    setTotal(userResult)
   } 
 
-  componentDidMount() {
-    const {selectEmissionFactors, distance, weight} = this.props.location.data
-    this.catchValue(selectEmissionFactors, distance, weight)
-  }
-
-  render() {
+  useEffect(()=> {
+    try{
+      const {selectEmissionFactors, distance, weight} = props.location.data 
+      catchValue(selectEmissionFactors, distance, weight)
+    }catch{
+      history.goBack()
+    }
+  },[])
+ 
 
   const logoResult="image-src/logoResult.png";
   const logoResult2="image-src/logoResult2.png";
-  let resultCalc= this.state.total > 150;
+  let resultCalc= total > 150;
 
     return (
       <div>
       <BodyResult/>
       <div className="result">
         <img className={resultCalc ? "logoResult" : "logoResult2"} src={resultCalc ? logoResult : logoResult2} alt={"coloration button for result is good or bad"} />
-        <p className="resultText">{this.state.total} </p>
+        <p className="resultText">{total} </p>
       </div>
         <Solution/>
       </div>
         );
-      }
     }
     
     export default Result;
